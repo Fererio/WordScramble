d4c6c1ac3ef4afa10e362562f175b592e7f579b2
Balaji Srinivas <balaji21srinivas@gmail.com>
2024-02-26 21:42:11 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Balaji Srinivas on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
